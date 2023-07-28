9f061f44510760aa95f0b90a3b720a47ced582bf
papaseyone <charleskumahntim@gmail.com>
2023-07-27 18:37:15 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 
-
+/**
+ * main - Entry point of the program
+ *
+ * This function initializes an integer array 'a', sets the value of its third
+ * element to 1024, and then prints the modified value after setting its third
+ * element to 98 using pointer arithmetic.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
 int a[5];
 int *p;
 
-
 a[2] = 1024;
 p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
+/*
+* Write your line of code here...
+* Remember:
+* - You are not allowed to use 'a'
+* - You are not allowed to modify 'p'
+* - Only one statement allowed
+* - You are not allowed to code anything else than this line of code
+*/
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
