b7737303f4cb24a84d716297ff3aec2e48aac00d
papaseyone <charleskumahntim@gmail.com>
2023-10-26 08:28:58 -0700

    0x14. C - Bit manipulation

---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,4 @@
+0x14. C - Bit manipulation
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+Look for the right source of information without too much help
+How to manipulate bits and use bitwise operators
---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,18 @@
+_putchar.c
+
+
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the char c to stdout
+ * @c: char to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
+
