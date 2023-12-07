118ae74045442cf0d1dac5d5cd1f6e71646a4a95
papaseyone <charleskumahntim@gmail.com>
2023-12-07 09:27:21 -0800

    updated

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees dlistint_t list.
+ * @head: pointer to the  head of list
+ * Return: nothing
+ **/
+
+void free_dlistint(dlistint_t *head)
+{
+if (head == NULL)
+return;
+
+while (head->next)
+{
+head = head->next;
+free(head->prev);
+}
+free(head);
+}
