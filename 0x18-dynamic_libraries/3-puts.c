f55c386a859987ff9480802ce95730e991051afb
papaseyone <charleskumahntim@gmail.com>
2023-12-11 12:20:13 -0800

    updated

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+void _puts(char *str);
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This code prints the following message:
+ *
+ * "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
